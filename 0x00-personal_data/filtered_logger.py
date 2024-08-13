6824b865bdcc9913c87ada171a324200cb4b3676
altoid <noelreeds01@gmail.com>
2024-08-13 00:05:30 +0000

    obfuscates fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-"""returns the log message obfuscated"""
+"""returns the log message with obfuscated fields"""
+import logging
+import re
 
-def filter_datum():
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
+
+def filter_datum(fields, redaction, message, separator):
+    """obfuscates log message fields"""
+    for field in fields:
+        pattern_str = field +'=[^;]*'
+        repl_str = field +'='+redaction
+        print(re.sub(pattern_str, repl_str, message))
