4576eb80c15edd2249c21665dee3bb26f7f20655
altoid <noelreeds01@gmail.com>
2024-08-15 14:04:34 +0000

    edited PII fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import logging
 import re
 from typing import List
 
-PII_FIELDS = ('name', 'email', 'ssn', 'password', 'ip')
+PII_FIELDS = ('name', 'email', 'ssn', 'phone', 'ip')
 
 
 class RedactingFormatter(logging.Formatter):
