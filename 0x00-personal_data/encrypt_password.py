26125d8d49faab09650570c4db7df17c1ce137f2
altoid <noelreeds01@gmail.com>
2024-08-20 13:06:46 +0000

    checks for valid passwd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,12 @@ def hash_password(password: str) -> bytes:
     passwd = password.encode('utf-8')
     hashed_passwd = bcrypt.hashpw(passwd, bcrypt.gensalt())
     return hashed_passwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks for a valid password"""
+    password = password.encode('utf=8')
+    if bcrypt.checkpw(password, hashed_password):
+        return True
+    else:
+        return False
