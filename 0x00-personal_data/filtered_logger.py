98b053d132ae463eb7ee9311904ed1b8dea471d0
altoid <noelreeds01@gmail.com>
2024-08-20 11:07:53 +0000

    sorts syntax err

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,8 +62,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to a database"""
     config = {
         'user': os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        'password': os.getenv'PERSONAL_DATA_DB_PASSWORD', ''),
-        'host': os.getenv'PERSONAL_DATA_DB_HOST', 'localhost'),
+        'password': os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        'host': os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
         'database': os.getenv('PERSONAL_DATA_DB_NAME')
     }
     connector = mysql.connector.connect(**config)
