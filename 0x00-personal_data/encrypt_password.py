d9f3b1f1b6df1524d78f126cef3b56d109aa3292
altoid <noelreeds01@gmail.com>
2024-08-20 12:48:13 +0000

    ecrypts a password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""encrypts a password"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """hashes a passwd str to a byte str"""
+    passwd = password.encode('utf-8')
+    hashed_passwd = bcrypt.hashpw(passwd, bcrypt.gensalt())
+    return hashed_passwd
