e726387b1fcf0c23ecf00f06fe67ac01569fa176
altoids <noelreeds01@gmail.com>
2025-09-30 14:55:52 +0300

    sets strict_slashes to false

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthotrized() -> str:
     """GET /api/v1/unauthorized
     Return:
