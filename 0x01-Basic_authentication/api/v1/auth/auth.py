27f8b7421cee67ccfea7398ac61df00464646bc1
altoids <noelreeds01@gmail.com>
2025-09-30 16:18:05 +0300

    implement auth. mechanisms

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Auth. module"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth. class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth."""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """Auth. Header"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
+        return None
