f84f267fb1246b5c67a4859460c16217958e4c94
altoids <noelreeds01@gmail.com>
2025-10-01 10:24:35 +0300

    validates a request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,30 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+
+try:
+    auth = os.environ.get("AUTH_TYPE")
+    if auth:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+except Exception as e:
+    pass
+
+
+@app.before_request
+def before_request():
+    """Set up method"""
+    if not auth:
+        return
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, paths):
+        return
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
 
 
 @app.errorhandler(403)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,7 +20,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Auth. Header"""
-        return None
+        if not request:
+            return None
+        if request.headers.get("Authorization") is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""
