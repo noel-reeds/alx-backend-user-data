402c89feea9a09fdcdbc77af39e9831833a5364d
altoids <noelreeds01@gmail.com>
2025-10-01 18:30:18 +0300

    retrives basic credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Basic Auth. module"""
+import re
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic Auth. class"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts- Base64 part of Auth Header"""
+        if not authorization_header or type(authorization_header) != str:
+            return None
+        try:
+            assert authorization_header.startswith("Basic")
+            assert re.match(r'\b\w+\s', authorization_header)
+            return authorization_header.split(" ")[1]
+        except Exception as e:
+            return None
