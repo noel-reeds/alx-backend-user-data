88ce6dea0a5ef89b763a3bfb0092c9fbc0bc45c3
altoids <noelreeds01@gmail.com>
2025-10-10 17:31:18 +0300

    returns authenticated user as JSON reponse

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,7 +30,8 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == "me" and request.current_user is None:
         abort(404)
     if user_id == "me" and request.current_user:
-        return jsonify([user.to_json() for user in request.current_user])
+        for user in request.current_user:
+            return user.to_json()
     user = User.get(user_id)
     if user is None:
         abort(404)
