44f683efea7b70f920ca15f17c0fe3640770ef4d
altoids <noelreeds01@gmail.com>
2025-10-11 17:43:46 +0300

    references session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,7 +15,7 @@ class SessionAuth(Auth):
         if type(user_id) is not str:
             return None
         session_id = uuid.uuid4()
-        user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
@@ -24,4 +24,4 @@ class SessionAuth(Auth):
             return None
         if type(session_id) is not str:
             return None
-        return user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
