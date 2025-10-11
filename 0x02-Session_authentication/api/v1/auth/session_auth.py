10d65e47ee87bb6f3daae3a509e8c8b790667fcb
altoids <noelreeds01@gmail.com>
2025-10-11 15:09:31 +0300

    creates and returns a session_id for a user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """Session module"""
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session Authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for a user_id"""
+        if not user_id:
+            return None
+        if type(user_id) != str:
+            return None
+        session_id = uuid.uuid4()
+        user_id_by_session_id[session_id] = user_id
+        return session_id
