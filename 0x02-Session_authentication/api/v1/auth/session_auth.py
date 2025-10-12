08f5eb799722311d1651b8d33abd43373db18b58
altoids <noelreeds01@gmail.com>
2025-10-12 09:41:02 +0300

    retrieves user_id for a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,7 +14,7 @@ class SessionAuth(Auth):
             return None
         if type(user_id) is not str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
