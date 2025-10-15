165e5e758ae637b164e9627414229c4dde2ae983
altoids <noelreeds01@gmail.com>
2025-10-15 19:55:03 +0300

    auth to store sessions to db

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,6 +27,9 @@ try:
     elif auth_s == "session_exp_auth":
         from api.v1.auth.session_exp_auth import SessionExpAuth
         auth = SessionExpAuth()
+    elif auth_s = "session_db_auth":
+        from api.v1.auth.session_db_auth import SessionDBAuth
+        auth = SessionDBAuth()
     else:
         from api.v1.auth.auth import Auth
         auth = Auth()
