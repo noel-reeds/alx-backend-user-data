8954b56f2d371e12506fb1fa4837b7ededc37d54
altoids <noelreeds01@gmail.com>
2025-10-30 02:15:47 +0300

    registers a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password into bytes"""
     hashed_passwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_passwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {user.email} already exists.")
+        except NoResultFound as e:
+            hashed = _hash_password(password)
+            user = User(email=email, hashed_password=hashed)
+            self._db._session.add(user)
+            self._db._session.commit()
+            return user
