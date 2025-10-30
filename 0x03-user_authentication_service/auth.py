15271d24daa8bc8f706f8079683af0e249cb7897
altoids <noelreeds01@gmail.com>
2025-10-30 16:10:54 +0300

    create a session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,13 @@ class Auth:
             return False
         except NoResultFound as e:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session id for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound as e:
+            return
