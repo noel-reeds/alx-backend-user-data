04a73bea903ee6dc51c77bc3f845db01ba5f7b7e
altoids <noelreeds01@gmail.com>
2025-10-30 12:47:06 +0300

    endpoint to register a non-existent user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Application module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -10,6 +12,16 @@ def status():
     """Status of application"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """Register a user"""
+    try:
+        email = request.form.get("email", None)
+        password = request.form.get("password", None)
+        user = auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
