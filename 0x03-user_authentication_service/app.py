81d987c8b7ceecea6a49c949ec2f2c15e70ce858
altoids <noelreeds01@gmail.com>
2025-10-31 13:51:25 +0300

    fixes pep8

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,6 +24,7 @@ def users():
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route("/sessions", methods=['POST'], strict_slashes=False)
 def login():
     """Logs in a user"""
