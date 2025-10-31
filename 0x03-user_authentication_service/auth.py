0c4e1985fbadc8c3cb04698ba3be1ebbf0b954be
altoids <noelreeds01@gmail.com>
2025-11-01 02:10:16 +0300

    generates reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,15 @@ class Auth:
             return None
         except ValueError as e:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                raise ValueError
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except ValueError as e:
+            return None
